0c451edcaf22f6ba493e0cf38ff21c0092cd4a45
ShiyumeMeguri <102138304+ShiyumeMeguri@users.noreply.github.com>
2022-06-01 11:33:59 +0900

    初期化-0.1.9.9

---
+++ AssetRipperCore/Classes/AnimationClip/Clip/AclClip.cs
@@ -0,0 +1,17 @@
+﻿using AssetRipper.Core.IO.Asset;
+
+namespace AssetRipper.Core.Classes.AnimationClip.Clip
+{
+	public sealed class AclClip : IAssetReadable
+	{
+		public byte[] ClipData { get; set; }
+
+		public uint CurveCount { get; set; }
+		public void Read(AssetReader reader)
+		{
+			ClipData = reader.ReadByteArray();
+			reader.AlignStream();
+			CurveCount = reader.ReadUInt32();
+		}
+	}
+}
